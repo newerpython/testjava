a24ed5ab24aed82b21f6bfbc1ac55f5a9acfca02
mzs <1013056516@qq.com>
2020-12-03 13:19:24 +0800

    dalkjfa1

---
+++ src/main/java/com/mzs/demo/Demo1.java
@@ -0,0 +1,11 @@
+package com.mzs.demo;
+
+/**
+ * @author zhangjh
+ * @date 2020/12/3
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
